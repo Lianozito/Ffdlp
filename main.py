6a488fdcf763b4dba0a790c8adf59c66f66678b3
Lianozito <lianozito@gmail.com>
2026-02-09 04:06:19 -0300

    Add main menu loop with option selection.

---
+++ main.py
@@ -0,0 +1,9 @@
+while True:
+    print('1 - Download youtube video?')
+    print('2 - Cut video')
+    print('0 - Close program')
+
+    choice = (input('Choose an option: '))
+    if choice == '1':
+        link = input('Please paste your YouTube link: ')
+        print('Verify...')
